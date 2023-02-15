b2f529129ed96bbb66c5da4ccf89c3773fad6f61
Tom Bourton <thomas.bourton@faculty.ai>
2023-02-15 21:06:16 +0000

    add api test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,8 @@
 from typing import Generator
 
-import fastapi
 import pytest
+
+import fastapi
 from fastapi.testclient import TestClient
 
 from app import app as _app
@@ -20,3 +21,17 @@ def client(app: fastapi.FastAPI) -> Generator[TestClient, None, None]:
 
 def test_health(client: TestClient):
     assert client.get("/docs").status_code == 200
+
+
+def test_rank_happy(client: TestClient):
+    hand = "2H 3D 5S 9C KD"
+    resp = client.post("/rank", content=hand)
+    assert resp.status_code == 200
+
+    assert resp.text == "high card: King"
+
+
+def test_rank_unhappy(client: TestClient):
+    resp = client.post("/rank", content="foo")
+    assert resp.status_code == 500
+    assert resp.text == "high card: King"
