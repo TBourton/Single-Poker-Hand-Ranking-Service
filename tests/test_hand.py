444e2a6bc0c604f5d595d777e2fecf4513bcf56e
Tom Bourton <thomas.bourton@faculty.ai>
2023-02-15 18:23:28 +0000

    added unhappy case tests for from_string for hands

--- tests/test_hand.py
+++ tests/test_hand.py
@@ -11,22 +11,26 @@ def test_card_happy(suit: SuitT, value: ValueT):
     assert Card(suit=suit, value=value)
 
 
-@pytest.mark.parametrize("suit", ["foo", 1])
-@pytest.mark.parametrize("value", ["bar", 7])
+@pytest.mark.parametrize("suit", ["foo", 2, 7])
+@pytest.mark.parametrize("value", ["bar", "H", 1])
 def test_card_unhappy(suit, value):
     with pytest.raises(ValidationError):
         Card(suit=suit, value=value)
 
 
-def test_hand_happy():
-    card = Card(suit="H", value=2)
-    assert Hand(
-        card1=card,
-        card2=card,
-        card3=card,
-        card4=card,
-        card5=card,
-    )
+@pytest.mark.parametrize("suit", ALLOWED_SUITS)
+@pytest.mark.parametrize("value", ALLOWED_VALUES)
+def test_card_from_string_happy(suit: SuitT, value: ValueT):
+    string = f"{value}{suit}"
+    assert Card.from_string(string)
+
+
+@pytest.mark.parametrize("suit", ["foo", 2, 7])
+@pytest.mark.parametrize("value", ["bar", "H", 1])
+def test_card_from_string_unhappy(suit, value):
+    string = f"{value}{suit}"
+    with pytest.raises(ValidationError):
+        Card.from_string(string)
 
 
 def test_hand_unhappy():
@@ -39,3 +43,14 @@ def test_hand_unhappy():
             card4=card,
             card5=card,
         )
+
+
+def test_hand_happy():
+    card = Card(suit="H", value=2)
+    assert Hand(
+        card1=card,
+        card2=card,
+        card3=card,
+        card4=card,
+        card5=card,
+    )
