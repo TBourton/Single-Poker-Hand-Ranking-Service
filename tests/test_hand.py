9febbfd3a6edab1c2e49bc262c0af79acaf01010
Tom Bourton <thomas.bourton@faculty.ai>
2023-02-15 18:15:36 +0000

    added unhappy case tests for cards

--- tests/test_hand.py
+++ tests/test_hand.py
@@ -2,19 +2,40 @@ import pytest
 
 from pydantic import ValidationError
 
-from hand import ALLOWED_SUITS, ALLOWED_VALUES, Card, SuitT, ValueT
+from hand import ALLOWED_SUITS, ALLOWED_VALUES, Card, Hand, SuitT, ValueT
 
 
 @pytest.mark.parametrize("suit", ALLOWED_SUITS)
 @pytest.mark.parametrize("value", ALLOWED_VALUES)
-def test_card(suit: SuitT, value: ValueT):
-    print(suit, value)
-    print(help(Card))
-    _ = Card(suit=suit, value=value)
+def test_card_happy(suit: SuitT, value: ValueT):
+    assert Card(suit=suit, value=value)
 
 
 @pytest.mark.parametrize("suit", ["foo", 1])
 @pytest.mark.parametrize("value", ["bar", 7])
-def test_card_fail(suit, value):
+def test_card_unhappy(suit, value):
     with pytest.raises(ValidationError):
         Card(suit=suit, value=value)
+
+
+def test_hand_happy():
+    card = Card(suit="H", value=2)
+    assert Hand(
+        card1=card,
+        card2=card,
+        card3=card,
+        card4=card,
+        card5=card,
+    )
+
+
+def test_hand_unhappy():
+    card = Card(suit="H", value=2)
+    with pytest.raises(ValidationError):
+        Hand(
+            card1=7,
+            card2=card,
+            card3=card,
+            card4=card,
+            card5=card,
+        )
